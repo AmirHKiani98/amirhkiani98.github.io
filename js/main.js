9e2c6fc7322901d109408bc5f6c29d6bc4ec28b0
Amirhossein Kiani <amirhkiani98@yahoo.com>
2022-10-31 10:52:58 -0500

    Modify splide.js music cards

--- js/main.js
+++ js/main.js
@@ -7,23 +7,29 @@ for (let index = 0; index < skillBars.length; index++) {
 $(document).ready(function () {});
 
 // Splide Music
-$.ajax({
-  type: "POST",
-  url: "../php/getMusic.php",
-  dataType: "json",
-  success: function (response) {
-    let music = response;
-    let composed = music.composed;
-    let arranged = music.arranged;
-    composed.forEach((element) => {
-      let html = makeMusicCard(element.address, "Composed", element.name);
-      $("#ul-music").append($(html));
-    });
-    arranged.forEach((element) => {
-      let html = makeMusicCard(element.address, "Composed", element.name);
-      $("#ul-music").append($(html));
-    });
-    new Splide("#splide-music", {
+// $.ajax({
+//   type: "POST",
+//   url: "../php/getMusic.php",
+//   dataType: "json",
+//   success: function (response) {
+//     let music = response;
+//     let composed = music.composed;
+//     let arranged = music.arranged;
+//     composed.forEach((element) => {
+//       let html = makeMusicCard(element.address, "Composed", element.name);
+//       $("#ul-music").append($(html));
+//     });
+//     arranged.forEach((element) => {
+//       let html = makeMusicCard(element.address, "Composed", element.name);
+//       $("#ul-music").append($(html));
+//     });
+    
+//   },
+//   error: function (error) {
+//     console.log(error.responseText);
+//   },
+// });
+new Splide("#splide-music", {
       type: "slide",
       rewind: true,
       focus: "center",
@@ -57,104 +63,98 @@ $.ajax({
           audio.pause();
         });
     });
-  },
-  error: function (error) {
-    console.log(error.responseText);
-  },
-});
+// function makeMusicCard(address, type, name) {
+//   return `
+//     <li class="splide__slide">
+//         <div class="card" style="width:100%">
+//             <img class="card-img-top" src="${address}/image.png" alt="Card image cap">
+//             <div class="card-body">
+//                 <h5 class="card-title">${name}</h5>
+//                 <p style="font-size:10px" class="text-muted">${type}</p>
+//                 <div class="d-flex justify-content-center">
+//                     <audio controls preload='none'>
+//                         <source src="${address}/music.mp3" type="audio/mpeg">
+//                     </audio>
+//                 </div>
+//             </div>
+//         </div>
+//     </li>
+//     `;
+// }
 
-function makeMusicCard(address, type, name) {
-  return `
-    <li class="splide__slide">
-        <div class="card" style="width:100%">
-            <img class="card-img-top" src="${address}/image.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">${name}</h5>
-                <p style="font-size:10px" class="text-muted">${type}</p>
-                <div class="d-flex justify-content-center">
-                    <audio controls preload='none'>
-                        <source src="${address}/music.mp3" type="audio/mpeg">
-                    </audio>
-                </div>
-            </div>
-        </div>
-    </li>
-    `;
-}
+// // Splide Projects
 
-// Splide Projects
+// $.ajax({
+//   type: "POST",
+//   url: "../php/getProjects.php",
+//   dataType: "json",
+//   success: function (response) {
 
-$.ajax({
-  type: "POST",
-  url: "../php/getProjects.php",
-  dataType: "json",
-  success: function (response) {
+//     response.forEach((element) => {
+//       $("#ul-projects").append(
+//         getProjectHTML(
+//           element.projectName,
+//           element.programming_language,
+//           element.relative_time,
+//           element.color,
+//           element.caption
+//         )
+//       );
+//     });
+//     new Splide("#splide-projects", {
+//       type: "slide",
+//       rewind: true,
+//       focus: "center",
+//       trimSpace: false,
+//       arrows: false,
+//       perPage: 4,
+//       gap: "1rem",
+//       breakpoints: {
+//         1024: {
+//           perPage: 3,
+//           trimSpace: false,
+//           arrows: false,
+//         },
+//         767: {
+//           perPage: 2,
+//           trimSpace: false,
+//           arrows: false,
+//         },
+//         640: {
+//           perPage: 1,
+//           trimSpace: false,
+//           arrows: false,
+//         },
+//       },
+//     }).mount();
+//   },
+//   error: function (error) {
+//     console.log(error.responseText);
+//   },
+// });
 
-    response.forEach((element) => {
-      $("#ul-projects").append(
-        getProjectHTML(
-          element.projectName,
-          element.programming_language,
-          element.relative_time,
-          element.color,
-          element.caption
-        )
-      );
-    });
-    new Splide("#splide-projects", {
-      type: "slide",
-      rewind: true,
-      focus: "center",
-      trimSpace: false,
-      arrows: false,
-      perPage: 4,
-      gap: "1rem",
-      breakpoints: {
-        1024: {
-          perPage: 3,
-          trimSpace: false,
-          arrows: false,
-        },
-        767: {
-          perPage: 2,
-          trimSpace: false,
-          arrows: false,
-        },
-        640: {
-          perPage: 1,
-          trimSpace: false,
-          arrows: false,
-        },
-      },
-    }).mount();
-  },
-  error: function (error) {
-    console.log(error.responseText);
-  },
-});
-
-function getProjectHTML(
-  projectName,
-  programming_language,
-  relative_time,
-  color,
-  caption
-) {
-  return `<li class="splide__slide">
-        <div class="card h-100" style="width: 100%;">
-            <div class="card-body d-flex flex-column justify-content-between">
-                <h5 class="card-title" style="font-weight: bolder;"><a href="https://github.com/AmirHKiani98/${projectName}">${projectName}</a></h5>
-                <p class="card-text" style="font-size: 14px;">${caption}</p>
-                <div class="d-flex flex-row justify-content-between" id="project-detail">
-                    <div class="d-flex justify-content-center align-items-center">
-                        <div style="border-radius: 100%; width: 10px; height: 10px; ${color};"></div>
-                        <p class="m-2" style="font-size:12px">${programming_language}</p>
-                    </div>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <p class="m-2" style="font-size: 11px;">Last update ${relative_time}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </li>`;
-}
+// function getProjectHTML(
+//   projectName,
+//   programming_language,
+//   relative_time,
+//   color,
+//   caption
+// ) {
+//   return `<li class="splide__slide">
+//         <div class="card h-100" style="width: 100%;">
+//             <div class="card-body d-flex flex-column justify-content-between">
+//                 <h5 class="card-title" style="font-weight: bolder;"><a href="https://github.com/AmirHKiani98/${projectName}">${projectName}</a></h5>
+//                 <p class="card-text" style="font-size: 14px;">${caption}</p>
+//                 <div class="d-flex flex-row justify-content-between" id="project-detail">
+//                     <div class="d-flex justify-content-center align-items-center">
+//                         <div style="border-radius: 100%; width: 10px; height: 10px; ${color};"></div>
+//                         <p class="m-2" style="font-size:12px">${programming_language}</p>
+//                     </div>
+//                     <div class="d-flex justify-content-between align-items-center">
+//                         <p class="m-2" style="font-size: 11px;">Last update ${relative_time}</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </li>`;
+// }
